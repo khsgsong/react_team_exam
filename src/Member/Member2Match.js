45ae84cbaa79a6d0bb712e6aa7246fae885fe11a
naiana51 <92916376+naiana51@users.noreply.github.com>
2023-09-14 19:06:38 +0900

    intj 수정

--- src/Member/Member2Match.js
+++ src/Member/Member2Match.js
@@ -2,9 +2,60 @@ import "./member2Match.css";
 
 const Member2Match = () => {
   return (
-    <div>
-      <div>매치</div>
-    </div>
+    <>
+      <div className="match-wrap">
+        <div className="browser">궁합표 보러가기!</div>
+        <a>
+          <div>
+            <img
+              src="/img/member2/궁합intj-removebg-preview.png"
+              className="intj"
+            ></img>
+          </div>
+        </a>
+
+        <a>
+          <div>
+            <img
+              src="/img/member2/궁합enfp-removebg-preview.png"
+              className="enfp"
+            ></img>
+          </div>
+        </a>
+
+        <a>
+          <div>
+            <img
+              src="/img/member2/궁합entp-removebg-preview.png"
+              className="entp"
+            ></img>
+          </div>
+        </a>
+
+        <a>
+          <div>
+            <img
+              src="/img/member2/궁합istj-removebg-preview.png"
+              className="istj"
+            ></img>
+          </div>
+        </a>
+
+        <a>
+          <div>
+            <img
+              src="/img/member2/궁합isfj-removebg-preview-removebg-preview.png"
+              className="isfj"
+            ></img>
+          </div>
+        </a>
+
+        <div className="intjComment good1">짱이야!😁</div>
+        <div className="intjComment good2">사랑해!❤️</div>
+        <div className="intjComment bad1">꺼져!🖕</div>
+        <div className="intjComment bad2">퉤!👿</div>
+      </div>
+    </>
   );
 };
 
