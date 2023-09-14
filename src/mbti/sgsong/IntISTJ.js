e7a4d6a08a439b3f88e03f4a2d53a10e0d46e380
sgsong <142006448+khsgsong@users.noreply.github.com>
2023-09-14 14:15:49 +0900

    IntISTJ.js 추가

--- src/mbti/sgsong/IntISTJ.js
+++ src/mbti/sgsong/IntISTJ.js
@@ -5,12 +5,8 @@ const IntISTJ = () => {
     <div className="s-wrap">
       <div className="s-title">리액션 버튼 고장난 슬기봇</div>
       <div className="s-mbti">ISTJ</div>
-      <div className="s-img">
-        <img src="/img/member1.png" alt=""></img>
-      </div>
-      <div className="s-content-wrap">
-        <div className="s-content"></div>
-      </div>
+      <img className="s-img" src="/img/member1.png" alt=""></img>
+      <div className="s-score"></div>
     </div>
   );
 };
