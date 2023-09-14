dc85629c2de492d48d7da0c7966aaf018b775c91
JaeWook <142170487+pondue@users.noreply.github.com>
2023-09-14 14:15:45 +0900

    재욱MBTI

--- src/mbti/JaeWookComponent/Infp.js
+++ src/mbti/JaeWookComponent/Infp.js
@@ -4,7 +4,9 @@ const Infp = () => {
   return (
     <div className="content">
       <div>서브메뉴</div>
-      <div>내용</div>
+      <div className="mbti-title">INFP</div>
+      <div className="mbti-img-box">이미지</div>
+      <div>성격유형</div>
     </div>
   );
 };
---
+++ src/mbti/JaeWookComponent/JaeWookMain.js
@@ -0,0 +1,5 @@
+const JaeWookMain = () => {
+  return <div>재욱메인</div>;
+};
+
+export default JaeWookMain;
