3dba62f0e707c9b74ac46c5cdfdb9a7bdef39222
JaeWook <142170487+pondue@users.noreply.github.com>
2023-09-14 13:53:02 +0900

    재욱 임시경로 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./common/Main";
 import Member from "./common/Member";
+import Infp from "./mbti/JaeWookComponent/Infp";
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />}></Route>
         <Route path="/member" element={<Member />}></Route>
+        {/* 임시재욱MBTI이동 */}
+        <Route path="/infp" element={<Infp></Infp>}></Route>
       </Routes>
     </div>
   );
