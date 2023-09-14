915ac9e06003963c72d6e7cae9b79a2b6f4b619d
naiana51 <92916376+naiana51@users.noreply.github.com>
2023-09-14 18:17:06 +0900

    intj 페이지 수정

--- src/Member/Member2Me.js
+++ src/Member/Member2Me.js
@@ -5,12 +5,17 @@ const Member2Me = () => {
     <>
       <div className="main-img">
         <div className="name">
-          <span>고집만렙 명훈봇</span>
+          <span>인류애 박살 명훈봇</span>
         </div>
         <div className="type">
           <span>INTJ</span>
         </div>
         <img src="/img/intj-img.png"></img>
+        <div className="info">
+          <div className="name">이름 : 최명훈</div>
+          <div className="생년월일">생년월일 : 1991.02.05</div>
+          <div className="혈액형">혈액형 : AB형</div>
+        </div>
       </div>
     </>
   );
