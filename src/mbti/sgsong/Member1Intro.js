17145fed11a6a4170e569543e1b5b09662515b1a
sgsong <142006448+khsgsong@users.noreply.github.com>
2023-09-14 15:36:16 +0900

    index.html 구글 아이콘 링크 추가 외

--- src/mbti/sgsong/Member1Intro.js
+++ src/mbti/sgsong/Member1Intro.js
@@ -6,7 +6,15 @@ const Member1Intro = () => {
       <div className="s-title">리액션 버튼 고장난 슬기봇</div>
       <div className="s-mbti">ISTJ</div>
       <img className="s-img" src="/img/member1.png" alt=""></img>
-      <div className="s-score"></div>
+      <div className="s-mbit-info">
+        <div className="star-rate">
+          <span>계획력</span>
+          <span className="material-icons">star_rate</span>
+        </div>
+      </div>
+      <div className="s-tag">
+        #법 없이도 살아 #승부욕 주의 #노잼 주의(인정 못해)
+      </div>
     </div>
   );
 };
--- src/mbti/sgsong/Member1SideMenu.js
+++ src/mbti/sgsong/Member1SideMenu.js
@@ -1,9 +1,33 @@
+import { Link } from "react-router-dom";
 import "./member1SideMenu.css";
 
 const Member1SideMenu = () => {
+  const activeMenu = (e) => {
+    const menu = document.querySelectorAll(".member1-side-menu a");
+    menu.forEach((item) => {
+      item.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+  };
   return (
-    <div>
-      <h1>사이드메뉴바</h1>
+    <div className="member1-side-menu">
+      <ul>
+        <li>
+          <Link to="introduce" onClick={activeMenu}>
+            ISTJ 소개
+          </Link>
+        </li>
+        <li>
+          <Link to="board" onClick={activeMenu}>
+            ISTJ 모여라
+          </Link>
+        </li>
+        <li>
+          <Link to="chemistry" onClick={activeMenu}>
+            ISTJ와 최고 케미는?
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
